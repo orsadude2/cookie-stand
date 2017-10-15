335a090bae0caab04d8df2b18fc8f366e6dc1acd
orsadude2 <dtcomer@gmail.com>
2017-10-14 17:29:52 -0700

    added hours, fixed console log

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 'use strict';
 
 console.log('Hello owner Pat...');
+var storeHour = ['6:00 am','7:00 am','8:00 am','9:00 am','10:00 am','11:00 am','12:00 pm','1:00 pm','2:00 pm','3:00 pm','4:00 pm','5:00 pm','6:00 pm','7:00 pm','8:00 pm]'];
 var store1 = {
   name: 'First and Pike',
   minCust: 23,
   maxCust: 65,
   avgCookieSale: 6.3,
-  hourandCust: [ ],
-  avcookyperhour: [ ],
+  rndhourandCust: [ ],
+  avcookysoldperhour: [ ],
+  totalCookys: 0,
 };
 var j = 0;
 function calcCust(min,max){
   while (j < 15) {
     var rannum = (Math.floor(Math.random() * (max - min + 1) + min)).toFixed(2);
-    store1.hourandCust.push(rannum);
-    var avcook = rannum * 6.3;
+    store1.rndhourandCust.push(rannum);
+    var avcookysoldperhour = rannum * 6.3;
     var hour = 1 + j;
-    store1.avcookyperhour.push(avcook);
-    console.log('The random # of people is ' + store1.hourandCust[j]);
-    console.log(' The average # of cookies needed for hour ' + hour + ' is ' + avcook);
+    store1.avcookysoldperhour.push(avcookysoldperhour);
+    console.log('The random # of people is ' + store1.rndhourandCust[j]);
+    console.log(' The average # of cookies needed for ' + storeHour[j] + ' is ' + avcookysoldperhour);
     j++;
 
   }
